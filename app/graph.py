98dbf25f3622a0f47b78d48426b55a4431b8888d
Robin <robincross321@gmail.com>
2024-04-29 18:28:44 -0600

    Added graph.py functions to produce viewable graph using altair library

--- app/graph.py
+++ app/graph.py
@@ -1,5 +1,16 @@
-from altair import Chart
+from altair import Chart, X, Y, Color, Tooltip
+from pandas import DataFrame
 
 
-def chart(df, x, y, target) -> Chart:
-    pass
+def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
+    chart = Chart(df).mark_circle(size=60).encode(
+        x=X(x),
+        y=Y(y),
+        color=Color(target),
+        tooltip=Tooltip(df.columns.tolist())
+    ).properties(
+        width=600,
+        height=400,
+        title=f"{y} by {x} for {target}"
+    )
+    return chart
