b364d88f40628424d04fceb152c937b86006156f
Robin <robincross321@gmail.com>
2024-05-06 15:37:43 -0600

    Created Machine Class in machine.py

--- app/machine.py
+++ app/machine.py
@@ -1,17 +1,29 @@
+from pandas import DataFrame
+from sklearn.ensemble import RandomForestClassifier
+from datetime import datetime
+import joblib
+
+
 class Machine:
+    def __init__(self, df: DataFrame):
+        self.name = "Random Forest Classifier"
+        self.timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        target = df["Rarity"]
+        features = df.drop(columns=["Rarity"])
+        self.model = RandomForestClassifier()
+        self.model.fit(features, target)
 
-    def __init__(self, df):
-        pass
-
-    def __call__(self, feature_basis):
-        pass
+    def __call__(self, pred_basis: DataFrame):
+        prediction = self.model.predict(pred_basis)
+        probability = self.model.predict_proba(pred_basis).max()
+        return prediction[0], probability
 
     def save(self, filepath):
-        pass
+        joblib.dump(self, filepath)
 
     @staticmethod
     def open(filepath):
-        pass
+        return joblib.load(filepath)
 
     def info(self):
-        pass
+        return f"{self.name} initialized at {self.timestamp}"
